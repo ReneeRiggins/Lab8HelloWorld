e994ba0b41dd6e2d66a38d12e84eb90d85f72fa3
renee.riggins <renee.riggins@accenture.com>
2018-09-14 11:24:19 -0500

    Initial Commit

---
+++ solution/Lab8HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Lab8HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
